82b346dca1eb132948eebe5a4a7d0ee1bb6499f0
markoowik <serik.alisher100605@gmail.com>
2023-10-31 21:26:24 +0500

    Update 31.10.23 | 21:26

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,20 @@ $(document).ready(function () {
       "active"
     ); /*тоггл - при клике добавляет/убирает класс active*/
   });
+
+  let btn = $(".expand-btn");
+  let hiddenRow = $(".expandable");
+
+  $(".expand-btn").click(function () {
+    let index = btn.index(this);
+    hiddenRow.eq(index).toggleClass("hidden");
+
+    if (hiddenRow.is(":visible")) {
+      hiddenRow.eq(index).slideDown(300); // анимация открытия
+      $(btn).eq(index).attr("src", "./icons/+ (1).svg");
+    } else {
+      hiddenRow.eq(index).slideUp(300); // анимация закрытия
+      $(btn).eq(index).attr("src", "./icons/+.svg");
+    }
+  });
 });
